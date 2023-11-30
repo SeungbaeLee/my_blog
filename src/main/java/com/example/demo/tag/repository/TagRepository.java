48793c258e9a4445a9de9a7787be069e4d7d1a08
SeungbaeLee <aha224610@gmail.com>
2023-11-30 20:26:12 +0900

    Repository 기본 추가

---
+++ src/main/java/com/example/demo/tag/repository/TagRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.tag.repository;
+
+import com.example.demo.tag.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}
