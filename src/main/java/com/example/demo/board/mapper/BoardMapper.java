1542c79c77db1185ad7c378b9ddbe1e70fd1c281
SeungbaeLee <aha224610@gmail.com>
2023-11-30 21:17:11 +0900

    Board Mapper 추가(수정필요)

--- src/main/java/com/example/demo/board/mapper/BoardMapper.java
+++ src/main/java/com/example/demo/board/mapper/BoardMapper.java
@@ -1,7 +1,20 @@
 package com.example.demo.board.mapper;
 
+import com.example.demo.board.dto.BoardDto;
+import com.example.demo.board.entity.Board;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface BoardMapper {
+
+    Board boardPostToBoard(BoardDto.Post post);
+
+    Board boardPatchToBoard(BoardDto.Patch patch);
+
+    @Mapping(target = "comments", source = "commentList")
+    @Mapping(target = "member", source = "member.name")
+    @Mapping(target = "category", source = "category.name")
+    @Mapping(target = "tags", source = "tags")
+    BoardDto.Response boardToResponse(Board board);
 }
