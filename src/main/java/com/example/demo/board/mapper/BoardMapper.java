824bc40abd195d3e8c55b6297b46c4f169f857cc
SeungbaeLee <aha224610@gmail.com>
2023-12-01 22:41:18 +0900

    BoardMapper 작성.

--- src/main/java/com/example/demo/board/mapper/BoardMapper.java
+++ src/main/java/com/example/demo/board/mapper/BoardMapper.java
@@ -2,11 +2,35 @@ package com.example.demo.board.mapper;
 
 import com.example.demo.board.dto.BoardDto;
 import com.example.demo.board.entity.Board;
+import com.example.demo.comment.entity.Comment;
+import com.example.demo.tag.entity.BoardTag;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Mapper(componentModel = "spring")
 public interface BoardMapper {
+
+    //List<Comment>를 List<String>으로 명시적으로 변환
+    default List<String> mapCommentListToStringList(List<Comment> comments) {
+        return comments.stream()
+                .map(Comment::getContent)
+                .collect(Collectors.toList());
+    }
+
+    default List<String> mapBoardTagListToStringList(List<BoardTag> tags) {
+        return tags.stream()
+                .map(BoardTag::getTagName)
+                .collect(Collectors.toList());
+    }
+
+    default List<BoardTag> mapStringListToBoardTagList(List<String> tags) {
+        return tags.stream()
+                .map(tagName -> BoardTag.builder().tagName(tagName).build())
+                .collect(Collectors.toList());
+    }
 
     Board boardPostToBoard(BoardDto.Post post);
 
@@ -14,7 +38,7 @@ public interface BoardMapper {
 
     @Mapping(target = "comments", source = "commentList")
     @Mapping(target = "member", source = "member.name")
-    @Mapping(target = "category", source = "category.name")
+    @Mapping(target = "category", source = "category.categoryName")
     @Mapping(target = "tags", source = "tags")
     BoardDto.Response boardToResponse(Board board);
 }
