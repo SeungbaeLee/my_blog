5ddd092e76c10bc3a36baa3115cd92520f36e264
SeungbaeLee <aha224610@gmail.com>
2023-12-08 17:34:32 +0900

    Member Mapping

--- src/main/java/com/example/demo/member/mapper/MemberMapper.java
+++ src/main/java/com/example/demo/member/mapper/MemberMapper.java
@@ -1,7 +1,13 @@
 package com.example.demo.member.mapper;
 
+import com.example.demo.member.dto.MemberDto;
+import com.example.demo.member.entity.Member;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
+
+    Member memberPostToMember(MemberDto.Post post);
+
+    MemberDto.Response memberToResponse(Member member);
 }
