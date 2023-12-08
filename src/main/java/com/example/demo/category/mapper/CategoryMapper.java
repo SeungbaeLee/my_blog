da173d9d23eac761e74de5eee44a2100488f4d54
SeungbaeLee <aha224610@gmail.com>
2023-12-08 17:34:53 +0900

    Category Mapping

--- src/main/java/com/example/demo/category/mapper/CategoryMapper.java
+++ src/main/java/com/example/demo/category/mapper/CategoryMapper.java
@@ -1,7 +1,29 @@
 package com.example.demo.category.mapper;
 
+import com.example.demo.board.entity.Board;
+import com.example.demo.category.dto.CategoryDto;
+import com.example.demo.category.entity.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
+
+
+    Category categoryPostToCategory(CategoryDto.Post post);
+
+    Category categoryPatchToCategory(CategoryDto.Patch patch);
+
+
+    @Mapping(target = "boards", source = "boardList")
+    CategoryDto.Response categoryToResponse(Category category);
+
+    default List<String> mapBoardListToBoardNames(List<Board> boardList) {
+        return boardList.stream()
+                .map(Board::getTitle)
+                .collect(Collectors.toList());
+    }
 }
