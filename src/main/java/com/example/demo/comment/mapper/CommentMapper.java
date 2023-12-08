fac71118ea7a1910b8a2c352e91416d60372f07f
SeungbaeLee <aha224610@gmail.com>
2023-12-08 17:34:43 +0900

    Comment Mapping

--- src/main/java/com/example/demo/comment/mapper/CommentMapper.java
+++ src/main/java/com/example/demo/comment/mapper/CommentMapper.java
@@ -1,7 +1,18 @@
 package com.example.demo.comment.mapper;
 
+import com.example.demo.comment.dto.CommentDto;
+import com.example.demo.comment.entity.Comment;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
+
+    Comment commentPostToComment(CommentDto.Post post);
+
+    Comment commentPatchToComment(CommentDto.Patch patch);
+
+
+    @Mapping(target = "memberName", source = "member.name")
+    CommentDto.Response commentToResponse(Comment comment);
 }
