85bc0b853a02a9b4a2b5e068d1672d4f3384ead4
SeungbaeLee <aha224610@gmail.com>
2023-12-11 02:03:15 +0900

    feat: numberOfBoards 추가(카테고리에 속한 게시글 수)

--- src/main/java/com/example/demo/category/dto/CategoryDto.java
+++ src/main/java/com/example/demo/category/dto/CategoryDto.java
@@ -3,6 +3,7 @@ package com.example.demo.category.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public class CategoryDto {
     @Getter
     @Setter
     @AllArgsConstructor
+    @NoArgsConstructor
     public static class Post {
         @NotBlank(message = "카테고리 이름을 작성해주세요")
         private String categoryName;
@@ -30,6 +32,6 @@ public class CategoryDto {
     public static class Response {
         private long categoryId;
         private String categoryName;
-        private List<String> boards;
+        private long numberOfBoards;
     }
 }
