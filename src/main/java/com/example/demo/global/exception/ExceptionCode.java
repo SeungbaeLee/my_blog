e3213e26b9679f321de0567f362ba8ac7397ecaa
SeungbaeLee <aha224610@gmail.com>
2023-12-11 02:04:22 +0900

    feat: category 예외 추가

--- src/main/java/com/example/demo/global/exception/ExceptionCode.java
+++ src/main/java/com/example/demo/global/exception/ExceptionCode.java
@@ -4,7 +4,8 @@ import lombok.Getter;
 
 public enum ExceptionCode {
 
-    EMAIL_EXISTS(409,"회원가입이 완료된 이메일입니다.");
+    EMAIL_EXISTS(409,"회원가입이 완료된 이메일입니다."),
+    CATEGORY_EXISTS(409,"이미 존재하는 카테고리입니다.");
 
 
     @Getter
