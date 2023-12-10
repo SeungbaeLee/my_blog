82bc538b528ad1efdcdfe351348f8aa72378f2d9
SeungbaeLee <aha224610@gmail.com>
2023-12-10 23:09:23 +0900

    feat: memberCreate 추가

--- src/main/java/com/example/demo/member/service/MemberService.java
+++ src/main/java/com/example/demo/member/service/MemberService.java
@@ -1,9 +1,13 @@
 package com.example.demo.member.service;
 
+import com.example.demo.global.exception.BusinessException;
+import com.example.demo.global.exception.ExceptionCode;
 import com.example.demo.member.entity.Member;
 import com.example.demo.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,12 +20,30 @@ import java.util.Optional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    public Member createMember(Member member) {
 
-    //이메일 검증
+        //Email 중복 검사
+        Boolean newEmail = checkEmail(member.getEmail());
+        if (!newEmail) {
+            throw new BusinessException(ExceptionCode.EMAIL_EXISTS);
+        }
+        //비밀번호 암호화
+        String encryptedPassword = passwordEncoder.encode(member.getPassword());
+        //객체 생성
+        Member newMember = Member.builder()
+                .name(member.getName())
+                .email(member.getEmail())
+                .password(encryptedPassword)
+                .build();
+        //ORM을 사용해 생성된 객체 DB에 저장
+        return memberRepository.save(newMember);
+    }
+
+    //Email 중복 검사
     private Boolean checkEmail(String email) {
         Optional<Member> optionalMember = memberRepository.findByEmail(email);
-
         return optionalMember.isEmpty();
     }
 
