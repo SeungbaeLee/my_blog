42901ddc3e074fba29d58c0c68258e9044d57806
SeungbaeLee <aha224610@gmail.com>
2023-12-11 00:23:57 +0900

    feat: Auditable extends

--- src/main/java/com/example/demo/member/entity/Member.java
+++ src/main/java/com/example/demo/member/entity/Member.java
@@ -2,6 +2,7 @@ package com.example.demo.member.entity;
 
 import com.example.demo.board.entity.Board;
 import com.example.demo.comment.entity.Comment;
+import com.example.demo.global.auditable.Auditable;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -11,7 +12,7 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+public class Member extends Auditable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
