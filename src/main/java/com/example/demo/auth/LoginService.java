b16785e3cfe9116d0e9118fb86da59b8fd81830d
SeungbaeLee <aha224610@gmail.com>
2023-12-13 17:13:20 +0900

    feat: login service 추가

---
+++ src/main/java/com/example/demo/auth/LoginService.java
@@ -0,0 +1,29 @@
+package com.example.demo.auth;
+
+
+import com.example.demo.member.entity.Member;
+import com.example.demo.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 이메일이 존재하지 않습니다."));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().name())
+                .build();
+    }
+}
