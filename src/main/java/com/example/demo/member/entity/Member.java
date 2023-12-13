d7cab5a5750282d27e1b92f542c4ecbda4fde028
SeungbaeLee <aha224610@gmail.com>
2023-12-13 17:11:48 +0900

    feat: security 적용을 위한 column들 추가

--- src/main/java/com/example/demo/member/entity/Member.java
+++ src/main/java/com/example/demo/member/entity/Member.java
@@ -33,13 +33,30 @@ public class Member extends Auditable {
     @OneToMany(mappedBy = "member")
     private List<Comment> commentList = new ArrayList<>();
 
+    @Enumerated(EnumType.STRING)
+    private Role role;//ROLE_USER, ROLE_ADMIN
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;//GOOGLE,NAVER,KAKAO
+
+    private String refreshToken; // 리프레시 토큰
+
+    public void setRole(Role role) { //삭제예정
+        this.role = role;
+    }
 
     @Builder
-    public Member(String name, String email, String password, List<Board> boardList, List<Comment> commentList) {
+    public Member(String name, String email, String password, List<Board> boardList, List<Comment> commentList,Role role) {
+
         this.name = name;
         this.email = email;
         this.password = password;
         this.boardList = boardList;
         this.commentList = commentList;
+        this.role = role;
+    }
+
+    public void updateRefreshToken(String updateRefreshToken) {
+        this.refreshToken = updateRefreshToken;
     }
 }
