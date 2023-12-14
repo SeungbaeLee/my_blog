f60fe8ea084815727d7da05a814f3607f3c0e1cf
SeungbaeLee <aha224610@gmail.com>
2023-12-14 17:05:21 +0900

    Refactor: createBoard 수정(categoryName으로 category 찾게)

--- src/main/java/com/example/demo/board/service/BoardService.java
+++ src/main/java/com/example/demo/board/service/BoardService.java
@@ -2,6 +2,8 @@ package com.example.demo.board.service;
 
 import com.example.demo.board.entity.Board;
 import com.example.demo.board.repository.BoardRepository;
+import com.example.demo.category.entity.Category;
+import com.example.demo.category.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,19 +20,23 @@ import java.util.ArrayList;
 public class BoardService {
 
     private final BoardRepository boardRepository;
-
+    private final CategoryRepository categoryRepository;
+    //Create
     public Board createBoard(Board board) {
 
+        Optional<Category> optionalCategory = categoryRepository.findByCategoryName(board.getCategory().getCategoryName());
+        Category category = optionalCategory.orElseThrow(() -> new RuntimeException("해당하는 Category가 없습니다."));// 나중에 예외처리
         Board newBoard = Board.builder()
                 .title(board.getTitle())
                 .content(board.getContent())
                 .commentList(new ArrayList<>())
                 .tags(board.getTags())
-                .category(board.getCategory())
+                .category(category)
                 .build();
         return boardRepository.save(newBoard);
     }
 
+    //Read
     public Page<Board> readBoardsOfCategory(Pageable pageable, long categoryId) {
         Pageable pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), pageable.getSort());
         return boardRepository.findByCategory_CategoryId(pageRequest, categoryId);
